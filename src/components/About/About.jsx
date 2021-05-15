270a9264dac46618ce43579474d1e0986122f9a7
abdullah alsaidi <abdullah.alsaidi16@gmail.com>
2021-05-15 23:06:50 +0300

    remove unwanted text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -42,11 +42,10 @@ const About = () => {
                     'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphTwo ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                  {paragraphTwo }
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+                  {paragraphThree }
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
