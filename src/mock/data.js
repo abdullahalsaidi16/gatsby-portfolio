9458af1530195f85a369ee02c61db74c1cca2910
Abdullah <abdullah.alsaidi16@gmail.com>
2022-08-24 16:03:27 +0200

    Fix Typos and Update information

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,5 +1,8 @@
 import { nanoid } from 'nanoid';
 
+const currentDate = new Date()
+const experienceYears = currentDate.getFullYear() - 2018
+
 // HEAD DATA
 export const headData = {
   title: 'Abdullah Alsaidi site', // e.g: 'Name | Developer'
@@ -11,21 +14,32 @@ export const headData = {
 export const heroData = {
   title: 'Hi, my name is ',
   name: 'Abdullah Alsaidi',
-  subtitle: 'I am Data Scientist with Backend Developement expirence',
+  subtitle: 'I am Software Engineer with Data science Experience',
   cta: 'Know More',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'me.jpg',
-  paragraphOne: 'Full Stack Data Scientist with 2+ years of experience in working on productionizing Artificial intelligence applications, predictive modeling, and geospatial data analysis. in addition to building data and CI/CD pipelines for Machine learning applications.',
-  paragraphTwo: '',
+  paragraphOne: `Sotware engineer with ${experienceYears}+ years of experience in working on productionizing Artificial intelligence applications, predictive modeling, and geospatial data analysis. in addition to building data and CI/CD pipelines for Machine learning applications.`,
+  paragraphTwo: `Skills and Technologies: Clean Code Data Visualizatio Data Processing and Cleaning Pandas Docker  PostgreSQL Redis  TDD
+  Git
+  Agile
+  Django
+  Python`,
   paragraphThree: '',
   resume: 'https://drive.google.com/file/d/1ta1Tm5Rta9uQPg_79gpOgazjx_Z29Qmw/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
+    {
+      id: nanoid(),
+      img: 'oneRagtime.png',
+      title: "Developing New Features in Oneragtime Venture Capital Platform.",
+      info2: '',
+      url: 'https://www.oneragtime.com/'
+    },
   {
     id: nanoid(),
     img: 'movecar.png',
@@ -46,7 +60,7 @@ export const projectsData = [
     id: nanoid(),
     img: '',
     title: 'Robot Localization system using GSM network and SLAM',
-    info: 'The localization of moving targets in wireless sensor networks has been a very important problem in the past decade. Massive research has been dedicated to find solutions for this problem. Though there exist various methods to solve the problem, the Received Signal Level (RSL) method has proved to be one of the best. Technically this methods uses the signal strength levels from different anchor nodes as indicators to the corresponding position. However, building a mathematical model to link the position with the corresponding RSSLs is a very complicated impirical approach. For this reason we use machine learning algorithms to build the required mathematical model. Considering the tracking problem of moving targets to a be a successive application of localization algorithms, we make use of Kalman Filter and information available from the accelorometer of the moving target in order to obtain better accuracy. This project and the associated codes are part of my graduation project named "Localization and Tracking of a moving target in a wireless sensor network" in the department of Computer and Automation Engineering, Faculty of Mechanical and Electrical Engineering, Damasus University.',
+    info: 'Develop machine learning algorithms to build the Wireless Sensor Netowork mathematical model. Considering the tracking problem of moving targets to be a successive application of localization algorithms, we make use of the Kalman Filter and information available from the accelerometer of the moving target in order to obtain better accuracy. This project and the associated codes are part of my graduation project named "Localization and Tracking of a moving target in a wireless sensor network" in the department of Computer and Automation Engineering, Faculty of Mechanical and Electrical Engineering, Damasus University.',
     info2: '',
     url: 'https://www.instagram.com/p/CLxhE-KgWgC/',
     repo: 'https://github.com/abdullahalsaidi16/wsn_localization_tracking'
